69ba96453da5b5542cea43e3be5051cad33e431e
Saharat Thongin <was.dew4546@gmail.com>
2024-06-20 18:39:00 +0700

    setup global prefix

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { config } from 'dotenv';
 config({ path: `${getEnv()}` });
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('/api');
   await app.listen(process.env.PORT);
 }
 bootstrap();
