105bb35807c917b49b5027c7bec9793b4767f25e
Saharat Thongin <was.dew4546@gmail.com>
2024-06-20 18:39:27 +0700

    set global versioning
    default api version is 1

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import getEnv from './utils/get-env';
 import { config } from 'dotenv';
+import { VersioningType } from '@nestjs/common';
 
 config({ path: `${getEnv()}` });
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
+
   app.setGlobalPrefix('/api');
+
   await app.listen(process.env.PORT);
 }
 bootstrap();
