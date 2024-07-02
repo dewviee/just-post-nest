00911c77ebac113b02b21b73e6037bf34a510d4d
Saharat Thongin <was.dew4546@gmail.com>
2024-07-03 02:31:03 +0700

    Generate migration for Post entity

---
+++ migrations/1719948628957-generated.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Generated1719948628957 implements MigrationInterface {
+    name = 'Generated1719948628957'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "post_entity" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "content" character varying(500) NOT NULL, "created_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), CONSTRAINT "PK_58a149c4e88bf49036bc4c8c79f" PRIMARY KEY ("id"))`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "post_entity"`);
+    }
+
+}
