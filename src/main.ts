4ef83b100a46fb2af7cb7e4c5b87975e1b77eb0a
Saharat Thongin <was.dew4546@gmail.com>
2024-07-20 11:01:45 +0700

    Implement auto validation using validation pipe

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import getEnv from './utils/get-env';
 import { config } from 'dotenv';
-import { VersioningType } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 
 config({ path: `${getEnv()}` });
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
 
   app.enableVersioning({
     type: VersioningType.URI,
