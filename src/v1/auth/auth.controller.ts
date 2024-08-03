550d39321b8bd91a1b01b7091a7a115b885fd565
Saharat Thongin <was.dew4546@gmail.com>
2024-08-04 06:16:21 +0700

    feat(decorator): Implement Public Decorator
    - Implement Public Decorator to use in Public Path
    - Implement @Public with login register and refreshToken path

--- src/v1/auth/auth.controller.ts
+++ src/v1/auth/auth.controller.ts
@@ -4,6 +4,7 @@ import { RegisterDTO } from './dto/register.dto';
 import { LoginDTO } from './dto/login.dto';
 import { JWTService } from 'src/common/services/jwt.service';
 import { Request as RequestEx, Response as ResponseEx } from 'express';
+import { Public } from 'src/common/decorators/public.decorator';
 
 @Controller('auth')
 export class AuthController {
@@ -12,11 +13,13 @@ export class AuthController {
     private readonly jwtService: JWTService,
   ) {}
 
+  @Public()
   @Post('/register')
   async register(@Body() body: RegisterDTO) {
     return await this.authService.register(body);
   }
 
+  @Public()
   @Post('/login')
   async login(@Body() body: LoginDTO, @Response() response: ResponseEx) {
     const accessToken = await this.authService.login(body, response);
@@ -25,6 +28,7 @@ export class AuthController {
     });
   }
 
+  @Public()
   @Post('/refresh-token')
   async refreshToken(@Request() request: RequestEx) {
     return await this.authService.refreshToken(request);
