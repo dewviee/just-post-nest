e1020cebbd9952feeff213f5168a0c7cb5e78807
Saharat Thongin <was.dew4546@gmail.com>
2024-08-18 22:26:47 +0700

    feat(service): Implement post get feed service

--- src/v1/post/post.module.ts
+++ src/v1/post/post.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostEntity } from 'src/common/entities/post/post.entity';
+import { PostGetFeedService } from './post-get-feed.service';
 import { PostController } from './post.controller';
 import { PostService } from './post.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([PostEntity])],
   controllers: [PostController],
-  providers: [PostService],
+  providers: [PostService, PostGetFeedService],
 })
 export class PostModule {}
