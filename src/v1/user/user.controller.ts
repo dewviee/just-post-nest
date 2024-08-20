4d0fb2b82368263a0daacf43764ab75c97ec8967
Saharat Thongin <was.dew4546@gmail.com>
2024-08-20 09:04:42 +0700

    feat(api): Implement PUT change-user-password API
    - Implement PUT /user/change-password API

--- src/v1/user/user.controller.ts
+++ src/v1/user/user.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Put } from '@nestjs/common';
 import { User } from 'src/common/decorators/user.decorator';
 import { UserEntity } from 'src/common/entities/post/user.entity';
+import { ChangeUserPasswordDto } from './dto/change-password.dto';
 import { UserService } from './user.service';
 
 @Controller('user')
@@ -10,5 +11,13 @@ export class UserController {
   @Get('/profile')
   async getUserInfo(@User() user: UserEntity) {
     return user;
+  }
+
+  @Put('/change-password')
+  async changeUserPassword(
+    @Body() body: ChangeUserPasswordDto,
+    @User() user: UserEntity,
+  ) {
+    await this.userService.changeUserPassword(user.id, body.password);
   }
 }
--- src/v1/user/user.service.ts
+++ src/v1/user/user.service.ts
@@ -1,11 +1,18 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AccessTokenEntity } from 'src/common/entities/post/session-access-token.entity';
+import { UserEntity } from 'src/common/entities/post/user.entity';
 import { Equal, Repository } from 'typeorm';
+import { PasswordService } from '../../common/services/password.service';
 
+@Injectable()
 export class UserService {
   constructor(
     @InjectRepository(AccessTokenEntity)
     private readonly acTokenRepo: Repository<AccessTokenEntity>,
+    @InjectRepository(UserEntity)
+    private readonly userRepo: Repository<UserEntity>,
+    private readonly passwordService: PasswordService,
   ) {}
 
   async getUserInfoFromAccessToken(acTokenStr: string) {
@@ -36,5 +43,19 @@ export class UserService {
 
     const user = accessToken?.refreshToken?.user;
     return user;
+  }
+
+  async changeUserPassword(userID: string, newPassword: string) {
+    const [hashedPassword, user] = await Promise.all([
+      this.passwordService.hash(newPassword),
+      this.userRepo.findOneBy({ id: Equal(userID) }),
+    ]);
+
+    if (!user) {
+      throw new BadRequestException('user id not found');
+    }
+
+    user.password = hashedPassword;
+    return await this.userRepo.save(user);
   }
 }
