840b9120d71896896fdd89feb2c68d7a10985d61
Saharat Thongin <was.dew4546@gmail.com>
2024-08-22 13:11:07 +0700

    setup CORS
    - allow origin
    - credentials

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,11 @@ config({ path: `${getEnv()}` });
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  });
+
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
   app.enableVersioning({
